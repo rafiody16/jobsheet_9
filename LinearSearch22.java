7fde5d103d9f42aa47cef81103388f9f2e075e96
rafiody16 <rafiody16@gmail.com>
2023-10-26 18:28:51 +0700

    Implementasi No 2 & 3

--- LinearSearch22.java
+++ LinearSearch22.java
@@ -1,16 +1,35 @@
+import java.util.Scanner;
+
 public class LinearSearch22 {
     public static void main (String[] args)
     {
-        int [] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        Scanner sc = new Scanner(System.in);
+
+        int key;
         int hasil = 0;
 
+        System.out.print("Masukkan Jumlah Elemen Array: ");
+        int jmlArray = sc.nextInt();
+        int [] arrayInt = new int[jmlArray];
+
         for (int i = 0; i < arrayInt.length; i++) {
-            if (arrayInt[i] == key) {
+            System.out.printf("Masukkan Elemen Array Ke-"+i+" : ");
+            arrayInt[i] = sc.nextInt();
+        }
+        for (int i = 0; i < arrayInt.length; i++)
+        {
+            System.out.print("Masukkan Key yang Ingin Dicari: ");
+            key = sc.nextInt();
+             if (arrayInt[i] == key) {
                 hasil = i;
+                System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
+                break;
+            }
+            else
+            {
+                System.out.println("Key Tidak Ditemukan!");
                 break;
             }
         }
-        System.out.println("Key ada dalam array pada posisi indeks ke-"+hasil);
     }
 }
